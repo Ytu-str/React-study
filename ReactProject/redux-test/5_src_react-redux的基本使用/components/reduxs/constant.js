f16f5650c76fd6edab182d0b71cd6ca8c5632966
Ytu-str <1466779249@qq.com>
2021-10-09 11:00:33 +0800

    clean

--- ReactProject/redux-test/5_src_react-redux的基本使用/components/reduxs/constant.js
+++
@@ -1,6 +0,0 @@
-/**
- * 该模块是定义action对象中type类型的常量池
- */
-
-export const INCREMENT ='increment'
-export const DECREMENT ='decrement'
--- ReactProject/redux-test/9_src_最终版/reduxs/actions/perosn.js
+++
@@ -1,3 +0,0 @@
-import {ADD_PERSON} from '../constant'
-//创建增加一个人的action动作对象
-export const addPserson = (personObj)=>({type:ADD_PERSON,data:personObj})
